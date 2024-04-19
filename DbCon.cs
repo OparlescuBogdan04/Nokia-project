f4b470683f614a4d6c71adb1e1ec592629f0e33d
Rares-Andrei Vasiu <rares_vasiu03@yahoo.ro>
2024-04-19 11:49:06 +0300

    Added the connection file to db

---
+++ DbCon.cs
@@ -0,0 +1,116 @@
+private MySqlConnection connection;
+
+private string server;
+
+private string database;
+
+private string uid;
+
+private string password;
+
+public DBConnect()
+{
+
+	Initialize();
+
+}
+
+//Initialize values
+
+private void Initialize()
+{
+
+	server = "127.0.0.1";
+
+	database = "Resources";
+
+	uid = "postgres";
+
+	password = "4825";
+
+	string connectionString;
+
+	connectionString = "SERVER=" + server + ";PORT=5432;" + "DATABASE=" +
+
+	database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+
+
+	connection = new MySqlConnection(connectionString);
+
+}
+
+//open connection to database
+
+private bool OpenConnection()
+{
+	try
+
+	{
+
+		connection.Open();
+
+		return true;
+
+	}
+
+	catch (MySqlException ex)
+
+	{
+
+		//When handling errors, you can your application's response based
+
+		//on the error number.
+
+		//The two most common error numbers when connecting are as follows:
+
+		//0: Cannot connect to server.
+
+		//1045: Invalid user name and/or password.
+
+		switch (ex.Number)
+		{
+
+			case 0:
+				{
+					MessageBox.Show("Cannot connect to server.  Contact administrator");
+
+					break;
+				}
+
+			
+			case 1045:
+				{
+					MessageBox.Show("Invalid username/password, please try again");
+
+					break;
+				}
+		}
+		return false;
+
+	}
+
+}
+
+
+//Close connection
+
+private bool CloseConnection()
+{
+	try
+	{
+		connection.Close();
+
+		return true;
+
+	}
+
+	catch (MySqlException ex)
+	{
+
+		MessageBox.Show(ex.Message);
+
+		return false;
+
+	}
+}
