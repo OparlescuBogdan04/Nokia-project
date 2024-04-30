24779069dad7ed4bd8749b6abd14de6b8b610e80
BogdanOparlescu <bogdanoparlescu@yahoo.com>
2024-04-30 10:41:30 +0300

    Basic UI for the Main Window + Monitoring.xaml

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Nokia
         {
             InitializeComponent();
         }
+
+        private void _Monitoring_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
